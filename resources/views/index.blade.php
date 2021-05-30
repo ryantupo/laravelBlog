38e1d52cf60f2382e3d749557610523c3a3532f8
Ryan <ryantupo98@gmail.com>
2021-05-30 22:52:26 +0100

    17.05.2021 setting up front page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,12 @@
-Home
+@extends('layouts.app')
+
+@section('content')
+    <div>
+        <div>
+            <div>
+                
+            </div>
+        </div>
+    </div>    
+
+@endsection
